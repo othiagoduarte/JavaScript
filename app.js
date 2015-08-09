f38fccf50ec2fd248077bbb902fdf4a16ca8fe5f
Thiago Duarte <thiago-duarte@outlook.com>
2015-08-09 19:42:28 -0300

    Atualização.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /*Criando um servidor - Início*/
 
-var http    = require("http");
-var server  = http.createServer(function(request,response){
+var http = require("http");		
+var server = http.createServer(function (request, response) {
 
-	response.writeHead(200, {"Content-Type": "text/html"});
+	response.writeHead(200, { "Content-Type": "text/html" });
 	response.write("<h1>Hello World!</h1>");
 	response.end();
 
-}); 
+});
 
-server.listen(3000,function(){
+server.listen(3000, function () {
 	console.log("Servidor iniciado");
 });
 
